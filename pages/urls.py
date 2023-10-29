d55ea4f0bec9038c21b57b565b7d0dbf2aa27d00
Joseph Turner <josephisaacturner@gmail.com>
2023-10-28 21:21:07 -0600

    stylistic updates

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
     re_path(r'^file_explorer/(?P<path>.*)$', file_explorer, name='file_explorer'),
+    path("file_explorer/", file_explorer, name='file_explorer'),
 ]
