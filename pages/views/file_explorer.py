402746d4f415ac4e4eb8dbba38489daad6f473e9
Joseph Turner <josephisaacturner@gmail.com>
2023-10-28 20:47:35 -0600

    make file_explorer more robust

--- pages/views/file_explorer.py
+++ pages/views/file_explorer.py
@@ -1,6 +1,6 @@
 import os
 from django.conf import settings
-from django.http import HttpResponse, FileResponse
+from django.http import FileResponse, Http404
 from django.shortcuts import render
 
 def file_explorer(request, path=''):
@@ -8,6 +8,10 @@ def file_explorer(request, path=''):
 
     # Full path to the directory or file
     full_path = os.path.join(base_dir, path)
+
+    # Check if the path actually exists
+    if not os.path.exists(full_path):
+        raise Http404("File or directory not found")
 
     # Check if the path is a file
     if os.path.isfile(full_path):
@@ -25,4 +29,10 @@ def file_explorer(request, path=''):
         else:
             directories.append(entry)
 
-    return render(request, 'pages/file_explorer.html', {'path': path, 'files': files, 'directories': directories})
+    empty_directory = not files and not directories
+
+    return render(
+        request,
+        'pages/file_explorer.html',
+        {'path': path, 'files': files, 'directories': directories, 'empty_directory': empty_directory}
+    )
