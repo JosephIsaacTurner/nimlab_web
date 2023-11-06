9c90bdaa30619c52b3d7345a015535c5f25ff913
Joseph Turner <josephisaacturner@gmail.com>
2023-11-06 12:44:54 -0700

    change model tablenames

--- pages/models.py
+++ pages/models.py
@@ -8,6 +8,9 @@ class Dataset(models.Model):
     creation_date = models.CharField(max_length=255, blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
 
+    class Meta:
+        db_table = 'datasets'
+
     def __str__(self):
         return self.name
 
@@ -15,31 +18,33 @@ class Tag(models.Model):
     tag = models.CharField(max_length=255, blank=True, null=True)
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='tags')
 
+    class Meta:
+        db_table = 'tags'
+
 class Author(models.Model):
     author_email = models.CharField(max_length=255)
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='authors')
+
+    class Meta:
+        db_table = 'authors'
 
 class Contact(models.Model):
     contact_email = models.CharField(max_length=255)
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='contacts')
 
+    class Meta:
+        db_table = 'contacts'
+
 class CitationSrc(models.Model):
     citation = models.TextField()
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='citations_src')
+
+    class Meta:
+        db_table = 'citations_src'
 
 class CitationTo(models.Model):
     citation = models.TextField()
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='citations_to')
 
-class NiftiImage(models.Model):
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='nifti_images')
-    path = models.CharField(max_length=255, unique=True)
-    type = models.CharField(max_length=255, blank=True, null=True)
-    md5 = models.CharField(max_length=255, blank=True, null=True)
-    roi_size = models.IntegerField(blank=True, null=True)
-    mask = models.CharField(max_length=255, blank=True, null=True)
-
-class NiftiLinearized(models.Model):
-    nifti_image = models.ForeignKey(NiftiImage, on_delete=models.CASCADE, related_name='nifti_linearized')
-    voxel_id = models.CharField(max_length=255)
-    value = models.CharField(max_length=255)
+    class Meta:
+        db_table = 'citations_to'
