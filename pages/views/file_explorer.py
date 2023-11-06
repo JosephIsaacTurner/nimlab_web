c16ac3e3bcba253c4bbfc015e8adb83c04156806
Joseph Turner <josephisaacturner@gmail.com>
2023-11-06 15:21:26 -0700

    another change

--- pages/views/file_explorer.py
+++ pages/views/file_explorer.py
@@ -93,8 +93,8 @@ def file_explorer(request, path=''):
 
     empty_directory = not contents['files'] and not any(contents['directories'])
     context = {
-        'display_path': str(path+'/').replace("//","/") if path else root_dir,
-        'path': path, 'contents': contents, 'empty_directory': empty_directory,
+        'display_path': path if path else root_dir,
+        'path': str(path+'/').replace("//","/"), 'contents': contents, 'empty_directory': empty_directory,
         'root_dir': root_dir
     }
     return render(request, 'pages/file_explorer.html', context)
