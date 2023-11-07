28c3ffa655202c262a6bccee02f28065cdf00ce3
Joseph Turner <josephisaacturner@gmail.com>
2023-11-07 13:58:43 -0700

    adjust allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = "django-insecure-0peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2
 DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "172.22.74.101"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "172.22.74.101", 'http://172.22.74.101:8080', '172.22.74.101:8080']
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = [
